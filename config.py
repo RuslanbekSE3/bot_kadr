7382236b1b6258112253857cc0f4f00f18fb4bcc
RuslanbekSE3 <ibratmahmudjonovich@gmail.com>
2025-07-15 09:59:02 +0500

    add bot code

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = '7945574839:AAFlscEmaPHBDS0F_OxpHdq6bFbGbvpctZc'
+
+ADMINS = [
+    5090453986,  # пока можешь вставить свой user_id
+]
+GROUP_ID = -4814278102  # сюда вставь chat_id твоей группы
